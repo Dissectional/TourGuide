834b07a4c4c6dc2aaf11bc482d7e225a97d135d5
Dissectional <Dissectional@users.noreply.github.com>
2015-12-14 22:48:46 +1300

    Rewrite Eversong Woods
    Massive rewrite of Eversong Woods.
    Still needs Class specific quests for the small trip into Silvermoon

--- TourGuide_Horde/01_13_Eversong.lua
+++ TourGuide_Horde/01_13_Eversong.lua
@@ -1,9 +1,9 @@
 TourGuide:RegisterGuide("Eversong Woods (1-13)", "Ghostlands (13-20)", "Horde", function()
 return [[
-A Reclaiming Sunstrider Isle |QID|8325| |R|Blood Elf| |N|Magistrix Erona (38.02,21.00)|
-C Reclaiming Sunstrider Isle |QID|8325| |R|Blood Elf|
-T Reclaiming Sunstrider Isle |QID|8325| |R|Blood Elf|
-A Unfortunate Measures |QID|8326| |R|Blood Elf| |N|Magistrix Erona (38.02,21.00)|
+A Reclaiming Sunstrider Isle |QID|8325| |N|Magistrix Erona (38.02,21.00)|
+C Reclaiming Sunstrider Isle |QID|8325|
+T Reclaiming Sunstrider Isle |QID|8325|
+A Unfortunate Measures |QID|8326| |N|Magistrix Erona (38.02,21.00)|
 
 A Mage Training |QID|8328| |R|Blood Elf| |C|Mage| |N|Magistrix Erona (38.02,21.00)|
 T Mage Training |QID|8328| |R|Blood Elf| |C|Mage|
@@ -18,39 +18,47 @@ T Hunter Training |QID|9393| |R|Blood Elf| |C|Hunter|
 A Paladin Training |QID|9676| |R|Blood Elf| |C|Paladin| |N|Magistrix Erona (38.02,21.00)|
 T Paladin Training |QID|9676| |R|Blood Elf| |C|Paladin|
 
-A The Shrine of Dath'Remar |QID|8345| |R|Blood Elf|
-A Solanian's Belongings |N|Don't forget to equip the bag he gives you| |QID|8330| |R|Blood Elf|
-A A Fistful of Slivers |QID|8336| |R|Blood Elf|
-A Thirst Unending |QID|8346| |R|Blood Elf|
+A Well Watcher Solanian |QID|10073| |R|Blood Elf| |C|Warlock|
+T Well Watcher Solanian |QID|10073| |R|Blood Elf| |C|Warlock|
+N Well Watcher Solanian |N|If you are able to pick up this quest, get it. It is not available on the server this was tested on| |R|Blood Elf| |C|Mage,Priest,Rogue,Hunter,Paladin|
 
-C Unfortunate Measures |QID|8326| |R|Blood Elf|
-T Unfortunate Measures |QID|8326| |R|Blood Elf|
-A Report to Lanthan Perilon |QID|8327| |R|Blood Elf|
-T Report to Lanthan Perilon |QID|8327| |R|Blood Elf|
-A Aggression |QID|8334| |R|Blood Elf|
+A The Shrine of Dath'Remar |QID|8345|
+A Solanian's Belongings |N|Don't forget to equip the bag he gives you| |QID|8330|
+A A Fistful of Slivers |QID|8336|
+A Thirst Unending |QID|8346| |R| Blood Elf|
 
-C Aggression |QID|8334| |R|Blood Elf|
-C The Shrine of Dath'Remar |QID|8345| |R|Blood Elf|
-C Thirst Unending |QID|8346| |R|Blood Elf|
-C A Fistful of Slivers |QID|8336| |R|Blood Elf|
-C Solanian's Belongings |N|South near a crystal, Southwest near a pond, West near treants| |QID|8330| |R|Blood Elf|
+C Thirst Unending |QID|8346|  |R| Blood Elf|
+C Unfortunate Measures |QID|8326|
+C A Fistful of Slivers |QID|8336|
 
-T Thirst Unending |QID|8346| |R|Blood Elf|
-T A Fistful of Slivers |QID|8336| |R|Blood Elf|
-T The Shrine of Dath'Remar |QID|8345| |R|Blood Elf|
-T Solanian's Belongings |QID|8330| |R|Blood Elf|
-T Aggression |QID|8334| |R|Blood Elf|
+T Thirst Unending |QID|8346|  |R| Blood Elf|
+T A Fistful of Slivers |QID|8336|
+T Unfortunate Measures |QID|8326|
 
-A Felendren the Banished |QID|8335| |R|Blood Elf|
-C Felendren the Banished |QID|8335| |R|Blood Elf|
-T Felendren the Banished |QID|8335| |R|Blood Elf|
+A Report to Lanthan Perilon |QID|8327|
+T Report to Lanthan Perilon |QID|8327|
+A Aggression |QID|8334|
 
-A Aiding the Outrunners |QID|8347| |R|Blood Elf|
-A Tainted Arcane Sliver |L|20483| |O| |QID|8338| |R|Blood Elf|
-T Tainted Arcane Sliver |O| |QID|8338| |R|Blood Elf|
+N Solanian's Journal |N|Kill Tenders and Feral Tenders while collecting Solanian's belongings (35.4, 22.5)| |QO|Solanian's Journal: 1/1|
+N Solanian's Scrying Orb |N|Kill Tenders and Feral Tenders while collecting Solanian's belongings (37.7,24.9)| |QO|Solanian's Scrying Orb: 1/1|
+N Scroll of Scourge Magic |N|Kill Tenders and Feral Tenders while collecting Solanian's belongings (31.4,22.8)| |QO|Scroll of Scourge Magic: 1/1|
+C The Shrine of Dath'Remar |QID|8345| |N|(28.9, 19.5)|
+C Aggression |QID|8334|
 
-R Dawning Lane |R|Blood Elf|
-T Aiding the Outrunners |QID|8347| |R|Blood Elf|
+T The Shrine of Dath'Remar |QID|8345|
+T Solanian's Belongings |QID|8330|
+
+T Aggression |QID|8334|
+A Felendren the Banished |QID|8335|
+C Felendren the Banished |QID|8335| |N|At the top of the Falthrien Academy to the South-West (32.7, 25.6)|
+A Tainted Arcane Sliver |L|20483| |O| |QID|8338|
+H Use your Hearthstone
+T Felendren the Banished |QID|8335|
+
+T Tainted Arcane Sliver |O| |QID|8338|
+A Aiding the Outrunners |QID|8347|
+
+T Aiding the Outrunners |QID|8347|
 A Slain by the Wretched |QID|9704| |N|On the border of Eversong and Sunstrider Isle (40.42, 32.18)|
 T Slain by the Wretched |QID|9704| |N|Just to the southeast (42.00, 35.61)|
 A Package Recovery |QID|9705|
@@ -77,8 +85,8 @@ T Delivery to the North Sanctum |QID|8895| |N|Head south through the gate, then 
 A Malfunction at the West Sanctum |QID|9119|
 T Malfunction at the West Sanctum |QID|9119| |N|Follow the road west to the West Sanctum (36.66, 57.55)|
 A Arcane Instability |QID|8486|
-C Arcane Instability |QID|8486|
 C Darnassian Intrusions |QID|9352| |N|Look for shadowmelded nelves on the west side of the sanctum|
+C Arcane Instability |QID|8486|
 A Incriminating Documents |QID|8482| |U|20765| |N|From the item the stinking elf dropped.|
 T Darnassian Intrusions |QID|9352| |N|Back on the other side of West Sanctum (36.70, 57.48)|
 T Arcane Instability |QID|8486|
@@ -88,12 +96,11 @@ C Fish Heads, Fish Heads... |QID|8884| |N|Kill murlocs, ugh.|
 A Captain Kelisendra's Lost Rutters |QID|8887| |U|21776| |N|Keep thwacking murlocks till this drops.|
 T Fish Heads, Fish Heads... |QID|8884| |N|Back at the camp (30.16, 58.30)|
 A The Ring of Mmmrrrggglll |QID|8885|
+H Falconwing Square |N|Might have to walk :(|
 
 T Incriminating Documents |QID|8482| |N|Back at Falconwing Square (48.08, 46.05)|
 A The Dwarven Spy |QID|8483|
 C The Dwarven Spy |QID|8483| |N|Head back to North Sanctum (44.13, 53.40).  Talk to the dwarf, go inside, kick his beardy ass.|
-T The Dwarven Spy |QID|8483| |N|Back at Falconwing Square (48.10, 46.06)|
-A Fairbreeze Village |QID|9256|
 
 A Roadside Ambush |QID|9035| |N|Just to the south along the main road (45.23, 56.33)|
 T Roadside Ambush |QID|9035| |N|Follow the road north to the bridge (44.90, 61.03)|
@@ -103,14 +110,16 @@ T Soaked Pages |QID|9062|
 A Taking the Fall |QID|9064|
 T Taking the Fall |QID|9064| |N|To the east at Stillwhisper Pond (55.66, 54.46)|
 A Swift Discipline |QID|9066|
-
 A The Dead Scar |QID|8475| |N|East, at the far north end of the Dead Scar (50.26, 50.88)|
 
-C Swift Discipline |N|North of Fairbreeze Village along the road.  Meledor (44,60) and Ralen (45,56)| |QID|9066|
-T Swift Discipline |QID|9066|
-
+N Swift Discipline |N|Discipline Ralen (45,56)| |QO|Apprentice Ralen Disciplined: 1/1|
+N Swift Discipline |N|Discipline Meledor (44,60)| |QO|Apprentice Meledor Disciplined: 1/1|
+C Swift Discipline |QID|9066|
 C The Dead Scar |QID|8475|
+T Swift Discipline |QID|9066|
 T The Dead Scar |QID|8475|
+T The Dwarven Spy |QID|8483| |N|Back at Falconwing Square (48.10, 46.06)|
+A Fairbreeze Village |QID|9256|
 
 A Pelt Collection |QID|8491| |N|Follow the road to Fairbreeze Village (44.68, 69.67)|
 A The Wayward Apprentice |QID|9254|
@@ -118,9 +127,36 @@ T Fairbreeze Village |QID|9256|
 A Situation at Sunsail Anchorage |QID|8892|
 h Fairbreeze Village
 
+N I CAN HAS PELTS? |N|Kill any cats you come across for "Pelt Collection"|
 T Captain Kelisendra's Lost Rutters |QID|8887| |N|South of West Sanctum on the other side of the creek (36.34, 66.66)|
 A Grimscale Pirates! |QID|8886|
 A Lost Armaments |QID|8480|
+
+C The Ring of Mmmrrrggglll |QID|8885| |N|To the west, patrolling along the shore.  If you're a belf, use Arcane Torrent to stop Mmmrrrggglll from healing himself.|
+C Grimscale Pirates! |QID|8886|
+A Captain Kelisendra's Lost Rutters |QID|8887| |U|21776| |N|Keep thwacking murlocs till this drops.|
+T The Ring of Mmmrrrggglll |QID|8885| |N|Back at the camp of the fish hater (30.29, 58.46)|
+
+C Lost Armaments |QID|8480| |N|Find the boxes all over the Sunsail Anchorage to the south.|
+T Captain Kelisendra's Lost Rutters |QID|8887| |N|South of West Sanctum on the other side of the creek (36.34, 66.66)|
+T Grimscale Pirates! |QID|8886| |N|East at the camp (36.35, 66.67)|
+T Lost Armaments |QID|8480|
+A Wretched Ringleader |QID|9076|
+C Wretched Ringleader |N|At the very top of building at Sunsail Anchorage (32.83, 69.71).  Focus fire on him, grab his head and run out of there!| |QID|9076|
+C Situation at Sunsail Anchorage |QID|8892|
+T Wretched Ringleader |QID|9076| |N|Back at the camp (36.33, 66.85)|
+
+C Pelt Collection |QID|8491|
+
+T Pelt Collection |QID|8491|
+T Situation at Sunsail Anchorage |QID|8892| |N|Back at Fairbreeze Village (43.39, 70.82)|
+A Farstrider Retreat |QID|9359|
+A Ranger Sareyn |QID|9358|
+A Missing in the Ghostlands |QID|9144|
+
+T Ranger Sareyn |QID|9358| |N|To the east along the road (46.88, 71.80)|
+A Defending Fairbreeze Village |QID|9252|
+C Defending Fairbreeze Village |N|Southern half of the Dead Scar| |QID|9252|
 
 T The Wayward Apprentice |QID|9254| |N|To the southwest at the East Sanctum (54.28, 70.98)|
 A Corrupted Soil |QID|8487|
@@ -131,64 +167,33 @@ C Unexpected Results |QID|8488|
 T Unexpected Results |QID|8488|
 A Research Notes |QID|9255|
 
-T Research Notes |QID|9255| |N|Back at Fairbreeze Village (44.07, 70.70)|
-A Ranger Sareyn |QID|9358|
-A The Scorched Grove |QID|9258|
-A Saltheril's Haven |QID|9395|
+R Farstrider Retreat
+T Farstrider Retreat |QID|9359|
+A Amani Encroachment |QID|8476| |N|Follow the road east then south to the Farstrider Retreat (60.33, 62.72)|
+A The Spearcrafter's Hammer |QID|8477|
+A The Magister's Apprentice |QID|8888|
 
-N I CAN HAS PELTS? |N|Kill any cats you come across for "Pelt Collection"|
-T Saltheril's Haven |QID|9395| |N|Follow the road west (38.18, 73.51)|
-A The Party Never Ends |QID|9067|
-
-C The Ring of Mmmrrrggglll |QID|8885| |N|To the west, patrolling along the shore.  If you're a belf, use Arcane Torrent to stop Mmmrrrggglll from healing himself.|
-C Grimscale Pirates! |QID|8886|
-T The Ring of Mmmrrrggglll |QID|8885| |N|Back at the camp of the fish hater (30.29, 58.46)|
-
-C Lost Armaments |QID|8480| |N|Find the boxes all over the Sunsail Anchorage to the south.|
-T Grimscale Pirates! |QID|8886| |N|East at the camp (36.35, 66.67)|
-T Lost Armaments |QID|8480|
-A Wretched Ringleader |QID|9076|
-C Wretched Ringleader |N|At the very top of building at Sunsail Anchorage (32.83, 69.71).  Focus fire on him, grab his head and run out of there!| |QID|9076|
-C Situation at Sunsail Anchorage |QID|8892|
-T Wretched Ringleader |QID|9076| |N|Back at the camp (36.33, 66.85)|
-
-C Pelt Collection |QID|8491|
-
-T Situation at Sunsail Anchorage |QID|8892| |N|Back at Fairbreeze Village (43.39, 70.82)|
-A Missing in the Ghostlands |QID|9144|
-T Pelt Collection |QID|8491|
-T Ranger Sareyn |QID|9358| |N|To the east along the road (46.88, 71.80)|
-A Defending Fairbreeze Village |QID|9252|
-
-T Missing in the Ghostlands |QID|9144|
-A The Fallen Courier |QID|9147|
-C The Fallen Courier |QID|9147|
-T The Fallen Courier |QID|9147|
-A Delivery to Tranquillien |QID|9148|
-C Defending Fairbreeze Village |N|Southern half of the Dead Scar| |QID|9252|
-
-R Silvermoon City
-B [Suntouched Special Reserve] |N|Vinemaster Suntouched (80,58), for "The Party Never Ends"|
-
-R Eversong Woods
 T The Magister's Apprentice |N|Follow the road East (67,57)| |QID|8888|
 A Deactivating the Spire |QID|8889|
 A Where's Wyllithen? |QID|9394|
-C Deactivating the Spire |N|Keep an eye out for Magister Duskwither's Journal on your way up.| |QID|8889|
-A Abandoned Investigations |QID|8891|
 T Where's Wyllithen? |N|To tho North (68,46)| |QID|9394|
 A Cleaning up the Grounds |QID|8894|
 C Cleaning up the Grounds |QID|8894|
 T Cleaning up the Grounds |QID|8894|
 T Deactivating the Spire |QID|8889|
 A Word from the Spire |QID|8890|
+C Deactivating the Spire |N|Keep an eye out for Magister Duskwither's Journal on your way up.| |QID|8889|
+A Abandoned Investigations |QID|8891|
 
-R Farstrider Retreat
+R Silvermoon |N|Buy Suntouched Special Reserve from Vinemaster Suntouched in Silvermoon|
+N Suntouched Special Reserve |N| Buy Suntouched Special Reserve from Vinemaster Suntouched(79.5,58.5)|
+
+N Training?
+N Class Quests? |N|Watch this space|
+
 T Word from the Spire |QID|8890|
 T Abandoned Investigations |QID|8891|
-A Amani Encroachment |QID|8476| |N|Follow the road east then south to the Farstrider Retreat (60.33, 62.72)|
-A The Spearcrafter's Hammer |QID|8477|
-A The Magister's Apprentice |QID|8888|
+
 C The Spearcrafter's Hammer |N|To the southeast (70,72)| |QID|8477|
 A Zul'Marosh |QID|8479|
 C Zul'Marosh |N|Across the lake, upstairs in the hut (62, 80)| |QID|8479|
@@ -201,14 +206,22 @@ T Amani Encroachment |QID|8476|
 A Amani Invasion |U|23249| |O| |QID|9360|
 T Amani Invasion |O| |QID|9360|
 A Warning Fairbreeze Village |QID|9363|
-
+N Springpaw Appetizers |N|Buy Springpaw Appetizers from Zalene Firstlight (60.4,62.5)|
 H Fairbreeze Village
-T Warning Fairbreeze Village |QID|9363|
-B [Bundle of Fireworks] |N|From the General goods vendor, for "The Party Never Ends"|
-T Defending Fairbreeze Village |N|(47,72)| |QID|9252|
-A Powering our Defenses |N|At the runestone (42,85)| |QID|8490|
 
+T Warning Fairbreeze Village |QID|9363|
+T Research Notes |QID|9255| |N|Back at Fairbreeze Village (44.07, 70.70)|
+A Saltheril's Haven |QID|9395| |N|Abandon "The Party Never Ends" if you have it|
+A The Scorched Grove |QID|9258|
+A Powering our Defenses |N|At the runestone (42,85)| |QID|8490|
+T Defending Fairbreeze Village |N|(47,72)| |QID|9252|
+
+N Bundle of Fireworks |N|Buy Bundle of Fireworks from Halis Dawnstrider (44.0,70.3)|
+
+T Saltheril's Haven |QID|9395| |N|Follow the road west (38.18, 73.51)|
+A The Party Never Ends |QID|9067|
 T The Party Never Ends |N|The quest reward item, [Saltheril's Haven Party Invitation], just allows you to pick up the party goods on the tables at the party.| |QID|9067|
+
 T The Scorched Grove |N|(34,80)| |QID|9258|
 A A Somber Task |QID|8473|
 C A Somber Task |N|Make sure you kill Old Whitebark for his pendant| |QID|8473|
@@ -220,5 +233,11 @@ A Whitebark's Memory |QID|10166|
 C Whitebark's Memory |N|Bury the pendant (37,86) and fight his spirit to low health| |QID|10166|
 T Whitebark's Memory |QID|10166|
 T Powering our Defenses |QID|8490|
+
+T Missing in the Ghostlands |QID|9144|
+A The Fallen Courier |QID|9147|
+C The Fallen Courier |QID|9147|
+T The Fallen Courier |QID|9147|
+A Delivery to Tranquillien |QID|9148|
 ]]
 end)
